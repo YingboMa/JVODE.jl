349a8edb4dfd63eadb3bc8585d7729f4e4e22cb1
Yingbo Ma <mayingbo5@gmail.com>
2019-09-15 00:25:33 -0400

    Add constants

--- src/JVODE.jl
+++ src/JVODE.jl
@@ -6,6 +6,10 @@ using DiffEqBase: ODEProblem
 using Reexport: @reexport
 @reexport using DiffEqBase
 
+
+###
+### Types
+###
 @enum Solver::Bool ADAMS BDF
 @enum NLSolver::Bool FUNCTIONAL NEWTON
 @enum ITask::Bool NORMAL ONE_STEP
@@ -18,8 +22,7 @@ const Q_MAX       =  ADAMS_Q_MAX  # max value of q for either lmm
 const L_MAX       =  (Q_MAX+1)    # max value of L for either lmm
 const NUM_TESTS   =  5            # number of error test quantities
 
-
-mutable struct JVodeMem{uType,tType,Rtol,Atol,F,D}
+mutable struct JVMem{uType,tType,Rtol,Atol,F,D}
     f::F
     data::D
     solver::Solver
@@ -91,9 +94,9 @@ mutable struct JVodeMem{uType,tType,Rtol,Atol,F,D}
 
     # Arrays for Optional Input and Optional Output
 
-    #long int *cv_iopt::Int  /* long int optional input, output */
-    #real     *cv_ropt::Int  /* real optional input, output     */
-    function JVodeMem(prob::ODEProblem,
+    #long int *cv_iopt::Int  # long int optional input, output */
+    #real     *cv_ropt::Int  # real optional input, output     */
+    function JVMem(prob::ODEProblem,
                       ::Type{Rtol}=Float64, ::Type{Atol}=Float64) where {Rtol,Atol}
         @unpack f, u0, tspan, p = prob
         obj = new{typeof(u0),eltype(tspan),Rtol,Atol,typeof(f),typeof(p)}()
@@ -102,6 +105,118 @@ mutable struct JVodeMem{uType,tType,Rtol,Atol,F,D}
     end
 end
 
+###
+### Default Constants
+###
+const HMIN_DEFAULT     = 0.0
+const HMAX_INV_DEFAULT = 0.0
+const MXHNIL_DEFAULT   = 10
+const MXSTEP_DEFAULT   = 5000
+
+###
+### Routine-Specific Constants
+###
+begin
+    macro define(name, val)
+        esc(:(const $name = $val))
+    end
+
+    # CVodeDky */
+
+    @define FUZZ_FACTOR 100.0
+
+    # CVHin */
+
+    @define HLB_FACTOR 100.0
+    @define HUB_FACTOR 0.1
+    @define H_BIAS     0.5
+    @define MAX_ITERS  4
+
+    # CVSet */
+
+    @define CORTES 0.1
+
+    # CVStep return values */
+
+    @define SUCCESS_STEP      0
+    @define REP_ERR_FAIL     -1
+    @define REP_CONV_FAIL    -2
+    @define SETUP_FAILED     -3
+    @define SOLVE_FAILED     -4
+
+    # CVStep control constants */
+
+    @define PREDICT_AGAIN    -5
+    @define DO_ERROR_TEST     1
+
+    # CVStep */
+
+    @define THRESH 1.5
+    @define ETAMX1 10000.0
+    @define ETAMX2 10.0
+    @define ETAMX3 10.0
+    @define ETAMXF 0.2
+    @define ETAMIN 0.1
+    @define ETACF  0.25
+    @define ADDON  0.000001
+    @define BIAS1  6.0
+    @define BIAS2  6.0
+    @define BIAS3  10.0
+    @define ONEPSM 1.000001
+
+    @define SMALL_NST    10   # nst > SMALL_NST => use ETAMX3          */
+    @define MXNCF        10   # max no. of convergence failures during */
+    # one step try                           */
+    @define MXNEF         7   # max no. of error test failures during  */
+    # one step try                           */
+    @define MXNEF1        3   # max no. of error test failures before  */
+    # forcing a reduction of order           */
+    @define SMALL_NEF     2   # if an error failure occurs and         */
+    # SMALL_NEF <= nef <= MXNEF1, then       */
+    # reset eta =  MIN(eta, ETAMXF)          */
+    @define LONG_WAIT    10   # number of steps to wait before         */
+    # considering an order change when       */
+    # q==1 and MXNEF1 error test failures    */
+    # have occurred                          */
+
+    # CVnls return values */
+
+    @define SOLVED            0
+    @define CONV_FAIL        -1
+    @define SETUP_FAIL_UNREC -2
+    @define SOLVE_FAIL_UNREC -3
+
+    # CVnls input flags */
+
+    @define FIRST_CALL      0
+    @define PREV_CONV_FAIL -1
+    @define PREV_ERR_FAIL  -2
+
+    # CVnls other constants */
+
+    @define FUNC_MAXCOR 3  # maximum no. of corrector iterations   */
+    # for iter == FUNCTIONAL                */
+    @define NEWT_MAXCOR 3  # maximum no. of corrector iterations   */
+    # for iter == NEWTON                    */
+
+    @define CRDOWN 0.3 # constant used in the estimation of the   */
+    # convergence rate (crate) of the          */
+    # iterates for the nonlinear equation      */
+    @define DGMAX  0.3 # iter == NEWTON, |gamma/gammap-1| > DGMAX */
+    # => call lsetup                           */
+
+    @define RDIV        2  # declare divergence if ratio del/delp > RDIV  */
+    @define MSBP       20  # max no. of steps between lsetup calls        */
+
+    @define TRY_AGAIN  99  # control constant for CVnlsNewton - should be */
+    # distinct from CVnls return values            */
+end
+
+###
+### CVODE Implementation
+###
+function populate!(mem::JVMem, args...)
+end
 
 """
     jvode(f, u0, tspan; rtol=1e-3, atol=1e-6)
